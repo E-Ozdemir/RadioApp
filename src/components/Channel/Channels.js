32e2227ad33d8e0fb51b68e73256b8e5574fe578
Emre Özdemir <55084899+E-Ozdemir@users.noreply.github.com>
2022-02-26 01:09:01 +0100

    fontawesome and font family added

--- src/components/Channel/Channels.js
+++ src/components/Channel/Channels.js
@@ -1,90 +1,69 @@
-import React, { useState } from 'react'
-import './Channel.scss'
+import React, { useState } from "react";
+import "./Channel.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlus, faCircleMinus } from "@fortawesome/free-solid-svg-icons";
 
+const Channels = ({
+  channel,
+  setIsPlaying,
+  isPlaying,
+  setChannelName,
+  key
+}) => {
+  const [showImage, setShowImage] = useState(false);
+  function isRadioOpen() {
+    setShowImage(!showImage);
+    setIsPlaying(!isPlaying);
+    setChannelName(channel.name);
+  }
 
-
-
-
-const Channels = ({ channel, setIsPlaying, isPlaying, setChannelName, key }) => {
-
-    const [showImage, setShowImage] = useState(false);
-    function isRadioOpen() {
-        setShowImage(!showImage);
-        setIsPlaying(!isPlaying);
-        setChannelName(channel.name);
-    }
-
-    const withImage = () => {
-        return (
-            <div className='channel_wrapper_image'>
-                <div className='image_wrapper'>
-                 {/* Fontawesome */}
-                    
-                    <img className='radio_image' src={channel.image} alt="" />
-                     {/* Fontawesome */}
-                </div>
-                <div className='channel_wrapper'>
-                    <span>{channel.name}</span>
-                    <span>{channel.frequency}</span>
-                </div>
-
-            </div>
-        )
-    }
-    const withoutImage = () => {
-        return (
-            <div className='channel_wrapper'>
-                {/* <p>a{key}</p> */}
-                <p>{channel.name}</p>
-                <p>{channel.frequency}</p>
-
-            </div>
-        )
-    }
-
+  const withImage = () => {
     return (
-        <div className="channels" onClick={() => isRadioOpen()}>
-            {showImage ? withImage() : withoutImage()}
-
+      <div className="channel_wrapper_image">
+        <div className="image_wrapper">
+          <FontAwesomeIcon icon={faCirclePlus} />
+          <img className="radio_image" src={channel.image} alt="" />
+          <FontAwesomeIcon icon={faCircleMinus} />
         </div>
-    )
-}
+        <div className="channel_wrapper">
+          <span>{channel.name}</span>
+          <span>{channel.frequency}</span>
+        </div>
+      </div>
+    );
+  };
+  const withoutImage = () => {
+    return (
+      <div className="channel_wrapper">
+        {/* <p>a{key}</p> */}
+        <p>{channel.name}</p>
+        <p>{channel.frequency}</p>
+      </div>
+    );
+  };
 
-export default Channels
+  return (
+    <div className="channels" onClick={() => isRadioOpen()}>
+      {showImage ? withImage() : withoutImage()}
+    </div>
+  );
+};
 
+export default Channels;
 
+// <div className="channels" onClick={() => isRadioOpen()}>
+//     {showImage ? (
+//         <>
+//             <img src={channel.image} alt="" />
+//             <span>{channel.name}</span>
+//             <span>{channel.frequency}</span>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   // <div className="channels" onClick={() => isRadioOpen()}>
-        //     {showImage ? (
-        //         <>
-        //             <img src={channel.image} alt="" />
-        //             <span>{channel.name}</span>
-        //             <span>{channel.frequency}</span>
-
-
-        //         </>
-        //     ) : (
-        //         <>
-        //             <span>{channel.name}</span>
-        //             <span>{channel.frequency}</span>
-        //             <hr />
-        //         </>
-        //     )}
-        // </div>
+//         </>
+//     ) : (
+//         <>
+//             <span>{channel.name}</span>
+//             <span>{channel.frequency}</span>
+//             <hr />
+//         </>
+//     )}
+// </div>
--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -1,61 +1,51 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react';
-import Channels from '../Channel/Channels';
-import { getRequest } from '../../utils/HttpRequest';
-import './Radio.scss'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Channels from "../Channel/Channels";
+import { getRequest } from "../../utils/HttpRequest";
+import "./Radio.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPowerOff, faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function Radio() {
+  const [channelName, setChannelName] = useState();
+  const [radio, setRadio] = useState();
+  const [isPlaying, setIsPlaying] = useState(false);
 
-    const [channelName, setChannelName] = useState();
-    const [radio, setRadio] = useState();
-    const [isPlaying, setIsPlaying] = useState(false);
+  useEffect(() => {
+    getRequest(setRadio);
+  }, []);
 
-    useEffect(() => {
-        getRequest(setRadio)
-    }, [])
-
-    const footer = (isPlaying) => {
-        return (
-            isPlaying ? (
-                <div>
-                    <span id='playing-text'>CURRENTLY PLAYING</span>
-                    <div className=''>
-                        {channelName}
-                    </div>
-
-                </div>
-            ) : (
-                <div className='not-playing'>
-                    Click any channel to listen!
-                </div>
-            )
-        )
-    }
-    return (
-        <div className='main'>
-            <header className='radio_header'>
-                 {/* Fontawesome */}
-                <span>STATIONS</span>
-                 {/* Fontawesome */}
-                
-            </header>
-            <section className='channel_list'>
-                {radio?.map((channel, index) => {
-                    return (
-                        <Channels
-                            key={index}
-                            channel={channel}
-                            isPlaying={isPlaying}
-                            setIsPlaying={setIsPlaying}
-                            setChannelName={setChannelName} />
-                    )
-                })}
-            </section>
-            <footer>
-                {footer(isPlaying)}
-            </footer>
-        </div>
-    )
+  const footer = (isPlaying) => {
+    return isPlaying ? (
+      <div>
+        <span id="playing-text">CURRENTLY PLAYING</span>
+        <div className="">{channelName}</div>
+      </div>
+    ) : (
+      <div className="not-playing">Click any channel to listen!</div>
+    );
+  };
+  return (
+    <div className="main">
+      <header className="radio_header">
+        <FontAwesomeIcon icon={faAngleLeft} />
+        <span>STATIONS</span>
+        <FontAwesomeIcon icon={faPowerOff} />
+      </header>
+      <section className="channel_list">
+        {radio?.map((channel, index) => {
+          return (
+            <Channels
+              key={index}
+              channel={channel}
+              isPlaying={isPlaying}
+              setIsPlaying={setIsPlaying}
+              setChannelName={setChannelName}
+            />
+          );
+        })}
+      </section>
+      <footer>{footer(isPlaying)}</footer>
+    </div>
+  );
 }
-
-
